a52922d46c0a6906a719757d640712126ff232af
damrem <damien.remars@gmail.com>
2017-04-22 17:28:56 +0200

    limiting speed

--- Assets/Scripts/HeroControl.cs
+++ Assets/Scripts/HeroControl.cs
@@ -6,41 +6,32 @@ using Ext;
 
 public class HeroControl : MonoBehaviour {
 
+	public float maxSpeed = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 		int h = (int)Input.GetAxisRaw ("Horizontal");
 		int v = (int)Input.GetAxisRaw ("Vertical");
 
-//		Dbg.Log (this, "Control", h, v);
-
 		Rigidbody2D body = GetComponent<Rigidbody2D> ();
 
-//		Dbg.Log (this, "rotation", transform.rotation.eulerAngles);
+		Vector2 force = new Vector2 (h, 0);
 
-		Vector2 force = new Vector2 (h, 0) * 10;
-		if(force.magnitude>0)
-			Dbg.Log (this, "before force", force);
-
-//		Dbg.Log (this, "body.rotation", body.transform.rotation);
-
-//		force.Rotate(
-
-//		force += body.transform.rotation;
-
-//		Dbg.Log (this, "euler", body.transform.rotation.eulerAngles.z);
 		force = force.Rotate (body.transform.rotation.eulerAngles.z);
-		if(force.magnitude>0)
-			Dbg.Log (this, "after force", force);
-//		force.
-//		force = Quaternion.Ang
+		body.AddForce (force * 100);
+		if (body.velocity.magnitude > maxSpeed) {
+			body.velocity = body.velocity.normalized * maxSpeed;
 
-		body.AddForce (force);
+		}
+
+
+
 
 	}
 }
